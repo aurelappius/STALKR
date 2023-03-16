c0c396abc14a2bbce18372bcc5c3d5a43a6d6b7c
aurel <appiusa@student.ethz.ch>
2023-03-16 15:20:49 -0700

    fixed bug

--- examples/picamera_demo_3.py
+++ examples/picamera_demo_3.py
@@ -10,4 +10,6 @@ cap.set(cv2.CAP_PROP_FPS, 40)
 start = time.time()
 for i in range(400):
     ret, img = cap.read()
-print("Time for {0} frames: {1} seconds".format(frames, time.time() - start))
+    cv2.imshow('Picamera', img)
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
